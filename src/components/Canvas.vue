fb148f28bf75f93b5b6d3af5acc52fe54a65a5c0
My-Nhien Nguyen <mynhien.nguyen@valtech.com>
2021-02-16 21:49:05 +0100

    wip: multi-touch experimental draft

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -5,6 +5,8 @@
       @pointerdown="beginDrawing"
       @pointerup="stopDrawing"
       @pointermove="draw"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
     >
     </canvas>
   </div>
@@ -27,6 +29,7 @@ export default defineComponent({
       history: [] as Action[],
       redoStack: [] as Action[],
       currentAction: null as Action,
+      tpCache: [],
     };
   },
   mounted(): void {
@@ -37,7 +40,7 @@ export default defineComponent({
     this.vueCanvas = context;
     this.resizeCanvas();
 
-    window.addEventListener("resize", this.resizeCanvas) //TODO: resizing will reset entire canvas, drawing needs to be redrawn
+    window.addEventListener("resize", this.resizeCanvas); //TODO: resizing will reset entire canvas, drawing needs to be redrawn
   },
   methods: {
     beginDrawing(e: MouseEvent): void {
@@ -72,30 +75,30 @@ export default defineComponent({
       }
     },
     undo(): void {
-      if(this.history.length > 0) {
-      const lastAction: Action = this.history.pop();
-      this.redoStack.push(lastAction);
+      if (this.history.length > 0) {
+        const lastAction: Action = this.history.pop();
+        this.redoStack.push(lastAction);
 
-      //clear canvas and redraw it
-      this.vueCanvas.clearRect(
-        0,
-        0,
-        this.vueCanvas.canvas.width,
-        this.vueCanvas.canvas.height
-      );
-      this.history.forEach((action: Action) => {
-        action.execute();
-      });
+        //clear canvas and redraw it
+        this.vueCanvas.clearRect(
+          0,
+          0,
+          this.vueCanvas.canvas.width,
+          this.vueCanvas.canvas.height
+        );
+        this.history.forEach((action: Action) => {
+          action.execute();
+        });
       }
     },
     redo(): void {
-      if(this.redoStack.length > 0) {
+      if (this.redoStack.length > 0) {
         const lastAction: Action = this.redoStack.pop();
         this.history.push(lastAction);
         lastAction.execute();
       }
     },
-    resizeCanvas(): void{
+    resizeCanvas(): void {
       // look up the size the canvas is being displayed
       const canvas: HTMLCanvasElement = document.getElementById(
         "canvas"
@@ -107,6 +110,53 @@ export default defineComponent({
       if (canvas.width !== width || canvas.height !== height) {
         canvas.width = width;
         canvas.height = height;
+      }
+    },
+    handleTouchStart(ev: TouchEvent) {
+      ev.preventDefault();
+      // Check this event for 2-touch Move/Pinch/Zoom gesture
+      if (ev.targetTouches.length == 2) {
+        for (let i = 0; i < ev.targetTouches.length; i++) {
+          this.tpCache.push(ev.targetTouches[i]);
+        }
+      }
+    },
+    handleTouchMove(ev) {
+      ev.preventDefault();
+      if (!(ev.touches.length == 2 && ev.targetTouches.length == 2))
+        // Check this event for 2-touch Move/Pinch/Zoom gesture
+        this.handlePinchZoom(ev);
+    },
+    handlePinchZoom(ev) {
+      if (ev.targetTouches.length == 2 && ev.changedTouches.length == 2) {
+        console.log("pass")
+        // Check if the two target touches are the same ones that started
+        // the 2-touch
+        let point1 = -1,
+          point2 = -1;
+        for (let i = 0; i < this.tpCache.length; i++) {
+          if (this.tpCache[i].identifier == ev.targetTouches[0].identifier)
+            point1 = i;
+          if (this.tpCache[i].identifier == ev.targetTouches[1].identifier)
+            point2 = i;
+        }
+        if (point1 >= 0 && point2 >= 0) {
+          // Calculate the difference between the start and move coordinates
+          const diff1 = Math.abs(
+            this.tpCache[point1].clientX - ev.targetTouches[0].clientX
+          );
+          const diff2 = Math.abs(
+            this.tpCache[point2].clientX - ev.targetTouches[1].clientX
+          );
+
+          // This threshold is device dependent as well as application specific
+          const PINCH_THRESHOLD = ev.target.clientWidth / 10;
+          if (diff1 >= PINCH_THRESHOLD && diff2 >= PINCH_THRESHOLD)
+            console.log("pinch");
+        } else {
+          // empty tpCache
+          this.tpCache = [];
+        }
       }
     },
   },
