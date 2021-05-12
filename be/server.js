b639ad7ae8d023dd2c4d6518b92c0f2a11f2ca08
My-Nhien Nguyen <mynhien.nguyen@valtech.com>
2021-05-12 13:46:27 +0200

    build: dynamic port nr for heroku

--- be/server.js
+++ be/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Server } = require('ws');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // create HTTP server
 const server = express()
